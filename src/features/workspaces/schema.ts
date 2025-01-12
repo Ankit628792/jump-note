a55b61c752d8ccc77f8e6df8450c8ef1dcd8734f
Ankit628792 <ankit628792@gmail.com>
2025-01-12 21:42:07 +0530

    update workspace patch route

--- src/features/workspaces/schema.ts
+++ src/features/workspaces/schema.ts
@@ -9,3 +9,13 @@ export const createWorkSpaceSchema = z.object({
         .optional()
 })
 
+
+export const updateWorkSpaceSchema = z.object({
+    name: z.string().min(3, "Must be more than 3 characters").optional(),
+    image: z.union([
+        z.instanceof(File),
+        z.string().transform((value) => value.trim() === "" ? undefined : value)
+    ])
+        .optional()
+})
+
--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -1,13 +1,15 @@
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { ID, Query } from "node-appwrite";
-import { createWorkSpaceSchema } from "../schema";
+import { createWorkSpaceSchema, updateWorkSpaceSchema } from "../schema";
 import { sessionMiddleware } from "@/lib/session-middleware";
 import { MEMBER_ROLE } from "@/features/members/types";
 import { generateInviteCode } from "@/lib/utils";
 import { DATABASE_ID, IMAGES_BUCKET_ID, MEMBERS_ID, WORKSPACES_ID } from "@/config";
+import { getMember } from "@/features/members/utils";
 
 const createWorkSpaceMiddleware = zValidator("form", createWorkSpaceSchema)
+const updateWorkSpaceMiddleware = zValidator("form", updateWorkSpaceSchema)
 
 const app = new Hono()
     .get("/", sessionMiddleware, async (c) => {
@@ -79,6 +81,68 @@ const app = new Hono()
         )
 
         return c.json({ data: workspace })
+    })
+    .patch("/:workspaceId", sessionMiddleware, updateWorkSpaceMiddleware, async (c) => {
+        const databases = c.get("databases")
+        const user = c.get("user")
+        const storage = c.get("storage")
+        const { workspaceId } = c.req.param()
+
+        const { name, image } = c.req.valid("form");
+
+        const isWorkspace = await databases.getDocument(
+            DATABASE_ID,
+            WORKSPACES_ID,
+            workspaceId
+        )
+
+
+        if (!isWorkspace) {
+            return c.json({ error: "Workspace not found" }, 404)
+        }
+        const member = await getMember({
+            databases,
+            workspaceId,
+            userId: user.$id
+        })
+
+        if (!member || (member.role !== MEMBER_ROLE.ADMIN)) {
+            return c.json({ error: "Unauthorized access" }, 403)
+        }
+
+
+        let imageUrl: string | undefined;
+
+        if (image instanceof File) {
+            const storage = c.get("storage")
+            const uploadedFile = await storage.createFile(
+                IMAGES_BUCKET_ID,
+                ID.unique(),
+                image
+            )
+            const arrayBuffer = await storage.getFilePreview(
+                IMAGES_BUCKET_ID,
+                uploadedFile.$id
+            );
+
+            imageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`
+        }
+        else {
+            imageUrl = image;
+        }
+
+        const workspace = await databases.updateDocument(
+            DATABASE_ID,
+            WORKSPACES_ID,
+            workspaceId,
+            {
+                name,
+                imageUrl,
+            }
+        )
+
+        return c.json({ data: workspace })
+
     })
 
 export default app
