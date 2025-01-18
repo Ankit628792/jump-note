5ab73a553ce273c83b5358300b15156177acfc27
Ankit628792 <ankit628792@gmail.com>
2025-01-18 12:48:45 +0530

    get workspace members list api

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono';
 import { handle } from "hono/vercel"
 import auth from '@/features/auth/server/route'
 import workspaces from '@/features/workspaces/server/route'
+import members from '@/features/members/server/route'
 
 const app = new Hono().basePath("/api")
     .get("/hello", (c) => {
@@ -11,6 +12,7 @@ const app = new Hono().basePath("/api")
 const routes = app
     .route("/auth", auth)
     .route("/workspaces", workspaces)
+    .route("/members", members)
 
 
 export const GET = handle(app);
